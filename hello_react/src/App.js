01c545a1b597c5edb780fcac542818b3e8e481b5
Taylor <taylous99@gmail.com>
2020-01-18 23:33:03 +0900

    practice event handler

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Say from "./Say";
+// import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  return <Say />;
+    return <EventPractice />;
 };
 
 export default App;
--- hello_react/src/MyComponent.js
+++ hello_react/src/MyComponent.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // 함수형 구조
 // const MyComponent = ({ name, favoriteNumber, children }) => {
@@ -17,25 +17,29 @@ import PropTypes from "prop-types";
 
 // 클래스형 구조
 class MyComponent extends Component {
-  // defaultProps, propTypes는 class 내부에서도 선언가능
-  static defaultProps = {
-    name: "기본 이름"
-  };
-  static propTypes = {
-    name: PropTypes.string,
-    favoriteNumber: PropTypes.number.isRequired
-  };
-  render() {
-    const { name, favoriteNumber, children } = this.props;
-    return (
-      <div>
-        안녕하세요, 제 이름은 {name}입니다. <br />
-        children 값은 {children} 입니다.
-        <br />
-        제가 좋아하는 숫자는 {favoriteNumber}입니다.
-      </div>
-    );
-  }
+    // defaultProps, propTypes는 class 내부에서도 선언가능
+    static defaultProps = {
+        name: '기본 이름',
+    };
+
+    static propTypes = {
+        name: PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired,
+    };
+
+    render() {
+        const { name, favoriteNumber, children } = this.props;
+        return (
+            <div>
+                안녕하세요, 제 이름은 {name}
+                입니다. <br />
+                children 값은 {children} 입니다.
+                <br />
+                제가 좋아하는 숫자는 {favoriteNumber}
+                입니다.
+            </div>
+        );
+    }
 }
 
 // MyComponent.defaultProps = {
