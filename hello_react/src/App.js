52a3b058b016a2427de98b2209edd6d3682a00a8
Taylous <taylous99@gmail.com>
2020-01-20 16:41:07 +0900

    practice react hooks

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
 // import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+// import ScrollBox from './ScrollBox';
+// import IterationSample from './IterationSample';
+// import Counter from './Hooks/Counter';
+// import Info from './Hooks/Info';
+import Average from './Hooks/Average';
 
 // const App = () => {
 //     return <EventPractice />;
 // };
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <ScrollBox ref={ref => (this.scrollBox = ref)} />
-                <button onClick={() => this.scrollBox.scrollToBottom()}>
-                    맨 밑으로
-                </button>
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <ScrollBox ref={ref => (this.scrollBox = ref)} />
+//                 <button onClick={() => this.scrollBox.scrollToBottom()}>
+//                     맨 밑으로
+//                 </button>
 
-                <IterationSample />
-            </div>
-        );
-    }
-}
+//                 <IterationSample />
+//             </div>
+//         );
+//     }
+// }
+
+const App = () => {
+    // const [visible, setVisible] = useState(false);
+
+    return (
+        <div>
+            {/* <button
+				onClick={() => {
+					setVisible(!visible);
+				}}
+			>
+				{visible ? '숨기기' : '보이기'}
+			</button>
+			<hr />
+			{visible && <Info />}
+			<Counter /> */}
+            <Average />
+        </div>
+    );
+};
 
 export default App;
