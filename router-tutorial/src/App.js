40ce15c78eafd94125401bb1835c48e63ece08b5
Taylor <taylous99@gmail.com>
2020-01-28 00:29:11 +0900

    practice react-router

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import About from './components/base/About';
+import Home from './components/base/Home';
+import Profile from './components/user/Profile';
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
+    </div>
+  );
+};
+
+export default App;
