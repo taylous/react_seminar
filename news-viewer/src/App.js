c210fa264721988acdf0028cf18db3c2b0a1cad6
Taylor <taylous99@gmail.com>
2020-01-30 00:50:51 +0900

    news viewer project

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
-function App() {
-
-  const [data, setData] = useState(null);
-  
-
-  return (
-    
-  );
-}
+const App = () => {
+  return <Route path="/:category?" component={NewsPage} />;
+};
 
 export default App;
