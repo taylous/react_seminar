58c4df8a06cc934756e421f86b9745496f113ab1
Taylor <taylous99@gmail.com>
2020-01-29 23:14:40 +0900

    initiate news viewer project

---
+++ news-viewer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+
+  const [data, setData] = useState(null);
+  
+
+  return (
+    
+  );
+}
+
+export default App;
--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 import About from './components/base/About';
 import Home from './components/base/Home';
-import Profile from './components/user/Profile';
+import Profiles from './components/user/Profiles';
+import HistorySample from './components/sample/HistorySample';
 
 const App = () => {
   return (
@@ -16,16 +17,24 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({location}) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}/>
+      </Switch>
     </div>
   );
 };
