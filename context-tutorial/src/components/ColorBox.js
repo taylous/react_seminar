41335a14d50363dd373ac2572dcc363560715dfd
Taylor <taylous99@gmail.com>
2020-02-02 19:32:25 +0900

    finish context api

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
+
+  const { state } = useContext(ColorContext);
+
   return (
-    <ColorContext.Consumer>
-      {value => (
-        <div
-          style={{
+    // <ColorConsumer>
+    //   {value => (
+    <>
+      <div
+        style={{
             width: '64px',
             height: '64px',
-            background: value.color
+            // background: value.state.color
+            background: state.color
           }}
-        />
-      )}
-    </ColorContext.Consumer>
+      />
+      <div
+        style={{
+            width: '32px',
+            height: '32px',
+            // background: value.state.subcolor
+            background: state.subcolor
+          }}
+      />         
+    </>
+    //   )}
+    // </ColorConsumer>
   );
 };
 
