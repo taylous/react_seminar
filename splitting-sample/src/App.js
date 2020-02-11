a8ac42040097ae0b66769281ec1d467ddfb84a6c
Taylor <taylous99@gmail.com>
2020-02-12 00:27:12 +0900

    prepare server side rendering project

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import loadable from '@loadable/component';
+
+// const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), { fallback: <div>loading...</div> });
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Reacet!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {/* <Suspense fallback={<div>loading...</div>}> */}
+        {visible && <SplitMe />}
+        {/* </Suspense> */}
       </header>
     </div>
   );
