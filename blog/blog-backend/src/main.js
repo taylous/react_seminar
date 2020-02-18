86035c3da1b49474a204f571cf71dcdb1443aa54
Taylor <taylous99@gmail.com>
2020-02-18 23:37:14 +0900

    initiate mongodb

---
+++ blog/blog-backend/src/main.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+// router.get('/', ctx => {
+//   ctx.body = '홈';
+// });
+router.use('/api', api.routes()); // api 라우트 사용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// 라우터 파라미터 사용
+// router.get('/about/:name?', ctx => {
+//   const { name } = ctx.params;
+//   // name의 존재 유무에 따라 다른 결과 출력
+//   ctx.body = name ? `${name}의 소개` : '소개';
+// });
+
+// URL 쿼리 사용
+// router.get('/posts', ctx => {
+//   const { id } = ctx.query;
+//   // id의 존재 유무에 따라 다른 결과 출력
+//   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+// });
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
