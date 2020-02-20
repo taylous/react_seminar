dbbc155f38f5a6b27c2ece285812f80ff1090bcb
Taylor <taylous99@gmail.com>
2020-02-20 23:43:04 +0900

    implement jwt token

---
+++ blog/blog-backend/src/models/user.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드를 작성할 때는 화살표 함수가 아닌 function 키워드를 사용해야 한다.
+// 함수 내부에서 this에 접근해야 하기 때문
+UserSchema.methods.setPassword = async function(password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function(password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; // true / false
+};
+
+// 스태틱 메서드
+// 스태틱 함수에서 this는 모델을 가리킴(여기서는 User)
+UserSchema.statics.findByUsername = function(username) {
+  return this.findOne({ username });
+};
+
+UserSchema.methods.serialize = function() {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+UserSchema.methods.generateToken = function() {
+  const token = jwt.sign(
+    // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
+    {
+      expiresIn: '7d', // 7일 동안 유효함
+    },
+  );
+  return token;
+};
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
