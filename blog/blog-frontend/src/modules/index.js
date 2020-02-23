9d2c48e55e17eb170fa1eef2516361d8b8f786e3
Taylor <taylous99@gmail.com>
2020-02-23 22:45:26 +0900

    Implement user/register func

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
-import auth from './auth';
+import { all } from 'redux-saga/effects';
+
+// Import Reducers
+import auth, { authSaga } from './auth';
+import user, { userSaga } from './user';
+import loading from './loading'
 
 const rootReducer = combineReducers({
   auth,
+  loading,
+  user,
 });
+
+export function* rootSaga() {
+  yield all([authSaga(), userSaga()]);
+}
 
 export default rootReducer;
